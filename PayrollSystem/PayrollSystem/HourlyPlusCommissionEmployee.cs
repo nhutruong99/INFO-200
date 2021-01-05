d64baf9db9a061e4b8c06e8d45fc170ee0c345ac
Nhu Truong <76985081+nhutruong99@users.noreply.github.com>
2021-01-04 20:24:41 -0800

    Add files via upload

---
+++ PayrollSystem/PayrollSystem/HourlyPlusCommissionEmployee.cs
@@ -0,0 +1,89 @@
+﻿//Nhu Truong
+//3/17/2020
+//Final Exam Code
+//Code editor: Nhu Truong History Change: 3/17/20   Instructor: Chuck
+//This class will provided function of calculating the hourly plus comission employee hourly pay
+
+using System;
+
+namespace PayrollSystem
+{
+    public class HourlyPlusCommissionEmployee: CommissionEmployee
+    {
+        private decimal hourlyRate; //hours worked for the week
+        private decimal wage; //  wage per hour
+
+
+        // six-parameter constructor for the HourlyPlusComissionEmployee class that being called in the main program
+        public HourlyPlusCommissionEmployee(string firstName, string lastName,
+          string socialSecurityNumber, decimal grossSales,
+          decimal commissionRate, decimal hourlyRate, decimal hourlyWage)
+          : base(firstName, lastName, socialSecurityNumber,
+               grossSales, commissionRate)
+        {
+            HourlyRate = hourlyRate; // validates hourly rate
+            wage = hourlyWage;
+            
+        }
+        // property that gets and sets hourly employee's wage hour
+        public decimal Wage
+        {
+            get
+            {
+                return wage;
+            }
+            set
+            {
+                if (value < 0) // validation
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value),
+                       value, $"{nameof(wage)} must be >= 0");
+                }
+                wage = value;
+            }
+        }
+
+
+        // property that gets and sets 
+        // HourlyPlusComissionEmployee's hourly rate
+        public decimal HourlyRate
+        {
+            get
+            {
+                return hourlyRate;
+            }
+            set
+            {
+                if (value < 0 || value > 168) // validation
+                {
+                    throw new System.ArgumentOutOfRangeException(nameof(value),
+                       value, $"{nameof(HourlyRate)} must be >= 0 and <= 168");
+                }
+
+                HourlyRate = value;
+            }
+
+        }
+        // calculate earnings with hour rate
+        public override decimal Earnings()
+        {
+            if (HourlyRate <= 40) // no overtime                          
+            {
+                return Wage * HourlyRate;
+            }
+            else
+            {
+                return (40 * Wage) + ((HourlyRate - 40) * Wage * 1.5M);
+            }
+        }
+
+        // return string representation of HourlyPlusComissionEmployee
+        public override string ToString() =>
+           $"hourly rate salary {base.ToString()}\nhourly rate salary: {HourlyRate:C}";
+
+
+
+
+
+    }
+}
---
+++ PayrollSystem/PayrollSystem/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Fig. 12.9: PayrollSystemTest.cs
+// Employee hierarchy test app.
+namespace PayrollSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create derived-class objects
+            var salariedEmployee = new SalariedEmployee("John", "Smith",
+                "111-11-1111", 800.00M);
+            var hourlyEmployee = new HourlyEmployee("Karen", "Price",
+               "222-22-2222", 16.75M, 40.0M);
+            var commissionEmployee = new CommissionEmployee("Sue", "Jones",
+               "333-33-3333", 10000.00M, .06M);
+            var basePlusCommissionEmployee =
+               new BasePlusCommissionEmployee("Bob", "Lewis",
+               "444-44-4444", 5000.00M, .04M, 300.00M);
+            // /////////////////////////////////////////////////////
+            // adding a new type of employee where pay is calculated by 
+            // the inherited behavior of commissions and the included info for hourly
+            // Richard Sherman has been hired as a pro football commentator.
+            // The network will pay him $3500 per hour for a 1 hour show daily. 
+            // He also earns .04 commissions on half a million dollars
+            // from the sales of his personally ON-AIR worn bow-ties.
+            var hourlyPlusCommissionEmployee =
+               new HourlyPlusCommissionEmployee("Richard", "Sherman", 
+               "555-55-5555", 500_000.00M, .04M, 7.00M, 3500.00M);
+
+            Console.WriteLine("Employees processed individually:\n");
+
+            Console.WriteLine($"{salariedEmployee}\nearned: " +
+                $"{salariedEmployee.Earnings():C}\n");
+            Console.WriteLine(
+               $"{hourlyEmployee}\nearned: {hourlyEmployee.Earnings():C}\n");
+            Console.WriteLine($"{commissionEmployee}\nearned: " +
+                $"{commissionEmployee.Earnings():C}\n");
+            Console.WriteLine($"{basePlusCommissionEmployee}\nearned: " +
+                $"{basePlusCommissionEmployee.Earnings():C}\n");
+            Console.WriteLine($"{hourlyPlusCommissionEmployee}\nearned: " +
+                $"{hourlyPlusCommissionEmployee.Earnings():C}\n");
+
+            // create List<Employee> and initialize with employee objects
+            var employees = new List<Employee>() {salariedEmployee,
+                hourlyEmployee, commissionEmployee, basePlusCommissionEmployee, hourlyPlusCommissionEmployee};
+
+            Console.WriteLine("Employees processed polymorphically:\n");
+
+            // generically process each element in employees
+            foreach (var currentEmployee in employees)
+            {
+                Console.WriteLine(currentEmployee); // invokes ToString
+
+                // determine whether element is a BasePlusCommissionEmployee
+                if (currentEmployee is BasePlusCommissionEmployee)
+                {
+                    // downcast Employee reference to 
+                    // BasePlusCommissionEmployee reference
+                    var employee = (BasePlusCommissionEmployee)currentEmployee;
+
+                    employee.BaseSalary *= 1.10M;
+                    Console.WriteLine("new base salary with 10% increase is: " +
+                        $"{employee.BaseSalary:C}");
+                }
+                else if (currentEmployee is HourlyPlusCommissionEmployee)
+                {
+                    // downcast Employee reference to 
+                    // HourlyPlusCommissionEmployee reference
+                    var employee = (HourlyPlusCommissionEmployee)currentEmployee;
+
+                    employee.HourlyRate *= 1.10M;
+                    Console.WriteLine("new hourly rate with 10% increase is: " +
+                        $"{employee.HourlyRate:C}");
+                }
+
+                Console.WriteLine($"earned: {currentEmployee.Earnings():C}\n");
+            }
+
+            // get type name of each object in employees 
+            for (int j = 0; j < employees.Count; j++)
+            {
+                Console.WriteLine($"Employee {j+1} is a {employees[j].GetType()}");
+            }
+
+        }
+    }
+}
